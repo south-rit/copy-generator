475084e4e6eb0fb8c674762bd10097fa17465f79
Вафин Вадим Равилевич <vadim.vafin@south.rt.ru>
2019-12-26 09:31:54 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+###Install
+
+```bash
+go install github.com/south-rit/copy-generator
+```
+
+###Use
+Add comment to struct `//rit-generator:copy` and run 
+```bash
+copy-generator YOU_SOURCE_FILE
+```
---
+++ cmd/struct-copy-generator/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/rit-epc/copy-generator/bootstrap"
+	"github.com/rit-epc/copy-generator/parser"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var allStructs = flag.Bool("all", false, "generate marshaler/unmarshalers for all structs in a file")
+var specifiedName = flag.String("output_filename", "", "specify the filename of the output")
+
+func generate(fname string) (err error) {
+	fInfo, err := os.Stat(fname)
+	if err != nil {
+		return err
+	}
+
+	p := parser.Parser{AllStructs: *allStructs}
+	if err := p.Parse(fname, fInfo.IsDir()); err != nil {
+		return fmt.Errorf("error parsing %v: %v", fname, err)
+	}
+
+	var outName string
+	if fInfo.IsDir() {
+		outName = filepath.Join(fname, p.PkgName+"_copy.go")
+	} else {
+		if s := strings.TrimSuffix(fname, ".go"); s == fname {
+			return fmt.Errorf("filename must end in '.go'")
+		} else {
+			outName = s + "_copy.go"
+		}
+	}
+
+	if *specifiedName != "" {
+		outName = *specifiedName
+	}
+
+	g := bootstrap.Generator{
+		PkgPath: p.PkgPath,
+		PkgName: p.PkgName,
+		Types:   p.StructNames,
+		OutName: outName,
+	}
+
+	_ = g
+
+	if err := g.Run(); err != nil {
+		return fmt.Errorf("bootstrap failed: %v", err)
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	files := flag.Args()
+
+	if len(files) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	for _, fname := range files {
+		if err := generate(fname); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
